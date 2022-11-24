83b0aa98c8d0196de34d71a78cc5c6e241b0a84c
Leonardo de Faveri <faver_i@hotmail.com>
2022-11-24 18:31:45 -0300

    atualizando a seção de investimentos

--- src/pages/Despesas.jsx
+++ src/pages/Despesas.jsx
@@ -8,15 +8,15 @@ const Despesas = () => {
     <div className="m-2 md:10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category='Page' title="Despesas" />
       <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={despesasGrid.rowsData}
-        columns={despesasGrid.columnsData}
-        pageSize={5}
-        rowsPerPageOptions={[5, 10]}
+        <DataGrid
+          rows={despesasGrid.rowsData}
+          columns={despesasGrid.columnsData}
+          pageSize={5}
+          rowsPerPageOptions={[5, 10]}
         // checkboxSelection
-      />
-    </div>
-      
+        />
+      </div>
+
     </div>
   )
 }
--- src/pages/Investimentos.jsx
+++ src/pages/Investimentos.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import { Header } from '../components'
 
 const Investimentos = () => {
   return (
-    <div></div>
+    <div className="m-2 md:10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category='Page' title="Investimentos" />
+      {/* <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={}
+          columns={}
+          pageSize={5}
+          rowsPerPageOptions={[5, 10]}
+        // checkboxSelection
+        />
+      </div> */}
+
+    </div>
   )
 }
 
